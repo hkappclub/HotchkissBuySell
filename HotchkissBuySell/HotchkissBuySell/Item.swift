1e3d91eff5aac7b4f6603ae69ceb4de93084ace6
Daniel Kim <kdanielive@gmail.com>
2017-04-04 19:43:25 -0400

    Main View and related files

---
+++ HotchkissBuySell/HotchkissBuySell/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  HotchkissBuySell
+//
+//  Created by Daniel Kim on 4/4/17.
+//  Copyright © 2017 Daniel Kim. All rights reserved.
+//
+
+import Foundation
+struct Item {
+    var name : String
+    var cost : Double
+    var owner : String
+    var location : String
+}
